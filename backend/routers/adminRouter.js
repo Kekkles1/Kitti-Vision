c225be4f23e306ab93b007f4a95de2a8ac73e2a5
Fatima Mahmood <62808131+Kekkles1@users.noreply.github.com>
2023-12-11 00:19:23 +0500

    commit

--- backend/routers/adminRouter.js
+++ backend/routers/adminRouter.js
@@ -5,14 +5,14 @@ const bodyParser = require('body-parser');
 
 const router = express.Router();
 
-router.get("/", adminsController.getAllAdmins);             //wokrs
-router.post("/AddNewAdmin",adminsController.AddNewAdmin);   //works
-router.get("/GetAllUsers",adminsController.getAllUsers);    //works
-router.post("/AddNewShow",adminsController.AddNewShow);     //works
-router.delete("/DeleteAdminID",adminsController.DeleteAdminID); //works
-router.delete("/DeleteUserID",adminsController.DeleteUserID);    //doesnt work
+router.get("/", adminsController.getAllAdmins);                     //wokrs
+router.post("/AddNewAdmin",adminsController.AddNewAdmin);           //works
+router.get("/GetAllUsers",adminsController.getAllUsers);            //works
+router.post("/AddNewShow",adminsController.AddNewShow);             //works
+router.delete("/DeleteAdminID",adminsController.DeleteAdminID);     //works
+router.delete("/DeleteUserID",adminsController.DeleteUserID);       //doesnt work
 router.post("/AddNewEpisode",adminsController.AddNewEpisode);
-router.get("/getAllEpisodes",adminsController.getAllEpisodes);      //doesnt work
+router.get("/getAllEpisodes",adminsController.getAllEpisodes);              //doesnt work
 router.get("/AdminUsernameCheck",adminsController.AdminUsernameCheck);      //works
 router.get("/AdminPasswordCheck",adminsController.AdminPasswordCheck);      //works
 
--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -5,15 +5,15 @@ const bodyParser = require('body-parser');
 
 const router = express.Router();
 
-router.get("/", usersController.getAllUsers);           //works
-router.post("/AddNewUser",usersController.AddNewUser);  //works
-router.get("/GetAllTvShows",usersController.getAllTvShows);     //works
-router.get("/GetAllEpisodes",usersController.getAllEpisodes);       //doesnt work
+router.get("/", usersController.getAllUsers);                       //works
+router.post("/AddNewUser",usersController.AddNewUser);              //works
+router.get("/GetAllTvShows",usersController.getAllTvShows);         //works
 router.get("/GetAllWatchlists",usersController.getAllWatchlists);   //works
 router.get("/GetAllReviews",usersController.getAllReviews);         //works
 router.post("/AddReview",usersController.AddReview);                //works
 router.get("/GetShowName/:id",usersController.GetShowName);         //works
 router.get("/UsernameCheck",usersController.UsernameCheck);         //WORKS
 router.get("/PasswordCheck",usersController.PasswordCheck);         //works
+router.get("/GetAllEpisodes",usersController.getAllEpisodes);       //doesnt work
 
 module.exports = router;
